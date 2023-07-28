5eba816bc4e5b939c5a12f4e2b57130d7d07dcc2
nanka85 <nana.kvachadzejs@gmail.com>
2023-07-28 12:47:24 +0400

    hash and jwt

--- models/user.model.js
+++ models/user.model.js
@@ -1,37 +1,59 @@
-import { DataTypes, Model } from "sequelize";
-import { DBConnection } from "../config.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./path/to/your/UserModel.js";
 
-export class User extends Model {}
+// Function to hash the password before saving it to the database
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    password: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    sequelize: DBConnection,
-    modelName: "users",
+// Function to create a new user and save it to the database
+export const createUser = async (firstName, lastName, email, password) => {
+  const hashedPassword = hashPassword(password);
+
+  try {
+    const user = await UserModel.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    return user;
+  } catch (err) {
+    throw new Error("Error creating user");
   }
-);
+};
 
-(async () => {
-  await DBConnection.sync({ force: true });
-})();
+// Function to authenticate a user and generate a JWT token
+export const authenticateUser = async (email, password) => {
+  try {
+    const user = await UserModel.findOne({ where: { email } });
 
-export const UserModel = new User();
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      throw new Error("Invalid password");
+    }
+
+    // Generate a JWT token and return it
+    const token = jwt.sign({ userId: user.id }, jwtSecretKey, { expiresIn: "1h" });
+    return token;
+  } catch (err) {
+    throw new Error("Authentication failed");
+  }
+};
+
+// Function to verify a JWT token and return the user
+export const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, jwtSecretKey);
+    const userId = decoded.userId;
+    return UserModel.findByPk(userId);
+  } catch (err) {
+    throw new Error("Invalid token");
+  }
+};
